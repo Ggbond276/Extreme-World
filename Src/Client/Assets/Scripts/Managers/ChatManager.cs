86ddcfd1b86b29b0d21e7cc5765468fc7dd6c5c8
GGCoder <2103035673@qq.com>
2026-08-26 15:57:00 +0800

    feat(chat): 完成客户端聊天系统数据流与网络层搭建
    - ChatManager: 实现本地多频道消息缓存池(上限200条)与UI刷新事件派发
    - ChatService: 封装发送请求接口，完成 Response 闭环回调与 Notify 广播监听

---
+++ Src/Client/Assets/Scripts/Managers/ChatManager.cs
@@ -0,0 +1,81 @@
+﻿using Assets.Scripts.Services;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+
+namespace Assets.Scripts.Managers
+{
+    class ChatManager : Singleton<ChatManager>
+    {
+        private const int MAX_CHAT_CACHE_COUNT = 200;
+
+        // 全局本地数据池
+        public List<NChatMessage> AllMessages = new List<NChatMessage>(); // 综合频道
+        public List<NChatMessage> LocalMessages = new List<NChatMessage>();
+        public List<NChatMessage> WorldMessages = new List<NChatMessage>();
+        public List<NChatMessage> SystemMessages = new List<NChatMessage>();
+        public List<NChatMessage> TeamMessages = new List<NChatMessage>();
+        public List<NChatMessage> GuildMessages = new List<NChatMessage>();
+        public List<NChatMessage> PrivateMessages = new List<NChatMessage>(); // 私聊本地缓存
+
+        public UnityAction<ChatChannel> OnChatUpdated;
+        public UnityAction OnChatSendSuucee;
+
+        public void Init()
+        {
+            ChatService.Instance.Init();
+        }
+
+
+        //Service上行调用
+
+        /// <summary>
+        /// 将服务器推送下来的消息存入本地，触发UI刷新事件
+        /// </summary>
+        public void AddMessage(NChatMessage message)
+        {
+            if(message.Channel != ChatChannel.System)
+            {
+                AddToList(AllMessages, message);
+            }
+
+            // 2. 根据具体频道塞入对应列表
+            switch (message.Channel)
+            {
+                case ChatChannel.Local: AddToList(LocalMessages, message); break;
+                case ChatChannel.World: AddToList(WorldMessages, message); break;
+                case ChatChannel.System: AddToList(SystemMessages, message); break;
+                case ChatChannel.Team: AddToList(TeamMessages, message); break;
+                case ChatChannel.Guild: AddToList(GuildMessages, message); break;
+                case ChatChannel.Private: AddToList(PrivateMessages, message); break;
+            }
+
+            // 3. 抛出事件，通知 UI 刷新 (把变化的频道传过去，UI 可以根据当前所处的切页决定要不要刷新)
+            OnChatUpdated?.Invoke(message.Channel);
+        }
+
+        private void AddToList(List<NChatMessage> list, NChatMessage message)
+        {
+            list.Add(message);
+            if (list.Count > MAX_CHAT_CACHE_COUNT)
+                list.RemoveAt(0);
+        }
+
+        // UI下行调用
+        public void SendChat(ChatChannel channel, string content, int toId = 0, string toName = "")
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                MessageBox.Show("输入内容不可为空");
+                return;
+            }
+
+            ChatService.Instance.SendChatRequest(channel, content, toId, toName);
+        }
+    
+    }
+}
---
+++ Src/Client/Assets/Scripts/Services/ChatService.cs
@@ -0,0 +1,65 @@
+﻿using Assets.Scripts.Managers;
+using Network;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Services
+{
+    class ChatService : Singleton<ChatService>, IDisposable
+    {
+
+        // 订阅
+        public void Init()
+        {
+            // 订阅服务端推送的 ChatNotify
+            MessageDistributer.Instance.Subscribe<ChatNotify>(this.OnChatNotify);
+            // 订阅自己发送消息后的 Response 响应
+            MessageDistributer.Instance.Subscribe<ChatResponse>(this.OnChatResponse);
+        }
+        // 解除订阅
+        public void Dispose()
+        {
+            MessageDistributer.Instance.Unsubscribe<ChatNotify>(this.OnChatNotify);
+            MessageDistributer.Instance.Unsubscribe<ChatResponse>(this.OnChatResponse);
+        }
+        // 发送Request
+        public void SendChatRequest(ChatChannel channel, string content, int toId = 0, string toName = "" )
+        {
+            NetMessage message = new NetMessage();
+            message.Request = new NetMessageRequest();
+            message.Request.chatRequest = new ChatRequest();
+
+            NChatMessage chatMessage = new NChatMessage();
+            chatMessage.Channel = channel;
+            chatMessage.Message = content;
+            chatMessage.toId = toId;
+            chatMessage.toName = toName;
+
+            message.Request.chatRequest.Message = chatMessage;
+            NetClient.Instance.SendMessage(message);
+        }
+        // 接收Response
+        private void OnChatResponse(object sender, ChatResponse response)
+        {
+            if(response.Result == Result.Success)
+            {
+                ChatManager.Instance.OnChatSendSuucee?.Invoke();
+            } else
+            {
+                // 发送失败：弹出错误提示
+                Debug.LogError($"聊天发送失败: {response.Errormsg}");
+            }
+        }
+        // 接收Notify
+        private void OnChatNotify(object sender, ChatNotify notify)
+        {
+            NChatMessage msg = notify.Message;
+            ChatManager.Instance.AddMessage(msg);
+        }
+    }
+}
